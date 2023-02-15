4599a519b867f3694fe26aeb2ec6c3e48865baf7
anoju <zun5761@gmail.com>
2023-02-15 10:23:22 +0900

    scss 세팅팅

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar_nav">
+    <div>
+      <p>Logo</p>
+    </div>
+    <div>
+      <p>Settings</p>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.navbar_nav{
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid;
+  padding: 0 15px;
+  font-size: rem(16);
+}
+</style>
