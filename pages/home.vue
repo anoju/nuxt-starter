533dbd0995f40e9a1e6b26ea9c0db78d0efa35a0
anoju <zun5761@gmail.com>
2023-02-16 17:09:26 +0900

    NuxtLink

--- pages/home.vue
+++ pages/home.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
     <h2>home page</h2>
+    <ul>
+      <li>
+        <NuxtLink to="/" class="button line">move index</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="scss" coped>
-h2 {
-  font-size: 2rem;
-}
-p {
-  margin: 0;
-  color: blue;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
   <h1>Index page</h1>
+  <ul>
+    <li>
+      <NuxtLink to="/home" class="button line">move home</NuxtLink>
+    </li>
+  </ul>
 </template>
